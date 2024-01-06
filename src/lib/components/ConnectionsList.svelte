dafa6e5b8c3fc812fdaa2ce2a86f12718e917d76
alehoppai <aleh_belski@outlook.com>
2024-01-06 23:07:33 +0100

    refactor: moving connections list to separate component

---
+++ src/lib/components/ConnectionsList.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { Icon, PlusCircle, MinusCircle, EllipsisVertical } from "svelte-hero-icons"
+	import { createEventDispatcher } from "svelte"
+	import { dbCredsStore } from "$lib/store/dbCreds"
+
+	export let formVisible = false
+
+	const dispatch = createEventDispatcher()
+	let optionsVisibleFor: null | number = null
+</script>
+
+<div class="bg-slate-100 p-2 w-full border-r border-slate-500">
+	{#if !$dbCredsStore.creds.length}
+		<button
+			class="flex flex-row justify-center items-center w-full gap-2"
+			on:click={() => dispatch("toggleForm")}
+		>
+			<Icon src={formVisible ? MinusCircle : PlusCircle} class="w-6 h-6 text-slate-900" />
+			<span class="text-slate-900">New Connection</span>
+		</button>
+	{:else}
+		{#each $dbCredsStore.creds as connection, i}
+			<div
+				class="flex flex-row items-center justify-between relative"
+				class:bg-blue-400={$dbCredsStore.connected}
+				class:text-slate-100={$dbCredsStore.connected}
+			>
+				<span>
+					{connection.database} | {connection.host}:{connection.port}
+				</span>
+				<button
+					on:click={() =>
+						optionsVisibleFor == null ? (optionsVisibleFor = i) : (optionsVisibleFor = null)}
+				>
+					<Icon src={EllipsisVertical} class="w-6 h-6 text-slate-700" />
+				</button>
+
+				{#if optionsVisibleFor === i}
+					<div class="absolute bg-slate-100 top-full right-0 rounded-xl shadow-xl flex flex-col">
+						<button
+							class="px-4 py-2"
+							on:click={() => {
+								optionsVisibleFor = null
+							}}
+						>
+							<span class="text-blue-500">Connect</span>
+						</button>
+						<hr />
+						<button
+							class="px-4 py-2"
+							on:click={() => {
+								optionsVisibleFor = null
+							}}
+						>
+							<span class="text-red-500">Delete</span>
+						</button>
+					</div>
+				{/if}
+			</div>
+		{/each}
+	{/if}
+</div>
--- src/lib/components/DBSelector.svelte
+++ src/lib/components/DBSelector.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import { Icon, CircleStack, PlusCircle, MinusCircle, EllipsisVertical } from "svelte-hero-icons"
-	import { dbCredsStore } from "$lib/store/dbCreds"
+	import { Icon, CircleStack, PlusCircle, MinusCircle } from "svelte-hero-icons"
 	import ConnectionForm from "./ConnectionForm.svelte"
 	import ConnectionError from "./ConnectionError.svelte"
+	import ConnectionsList from "./ConnectionsList.svelte"
 
 	let formVisible = false
-	let optionsVisibleFor: null | number = null
 </script>
 
 <ConnectionError />
@@ -24,59 +23,7 @@
 	<div
 		class="flex flex-row rounded-b-2xl w-full h-full overflow-x-hidden overflow-y-auto flex-nowrap relative"
 	>
-		<div class="bg-slate-100 p-2 w-full border-r border-slate-500">
-			{#if !$dbCredsStore.creds.length}
-				<button
-					class="flex flex-row justify-center items-center w-full gap-2"
-					on:click={() => (formVisible = !formVisible)}
-				>
-					<Icon src={formVisible ? MinusCircle : PlusCircle} class="w-6 h-6 text-slate-900" />
-					<span class="text-slate-900">New Connection</span>
-				</button>
-			{:else}
-				{#each $dbCredsStore.creds as connection, i}
-					<div
-						class="flex flex-row items-center justify-between relative"
-						class:bg-blue-400={$dbCredsStore.connected}
-						class:text-slate-100={$dbCredsStore.connected}
-					>
-						<span>
-							{connection.database} | {connection.host}:{connection.port}
-						</span>
-						<button
-							on:click={() =>
-								optionsVisibleFor == null ? (optionsVisibleFor = i) : (optionsVisibleFor = null)}
-						>
-							<Icon src={EllipsisVertical} class="w-6 h-6 text-slate-700" />
-						</button>
-
-						{#if optionsVisibleFor === i}
-							<div
-								class="absolute bg-slate-100 top-full right-0 rounded-xl shadow-xl flex flex-col"
-							>
-								<button
-									class="px-4 py-2"
-									on:click={() => {
-										optionsVisibleFor = null
-									}}
-								>
-									<span class="text-blue-500">Connect</span>
-								</button>
-								<hr />
-								<button
-									class="px-4 py-2"
-									on:click={() => {
-										optionsVisibleFor = null
-									}}
-								>
-									<span class="text-red-500">Delete</span>
-								</button>
-							</div>
-						{/if}
-					</div>
-				{/each}
-			{/if}
-		</div>
+		<ConnectionsList {formVisible} on:toggleForm={() => (formVisible = !formVisible)} />
 		<ConnectionForm {formVisible} on:hideForm={() => (formVisible = false)} />
 	</div>
 </div>
